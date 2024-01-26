4ffa989f15203b556c90f2981ddc44b24e8c6515
alisayed987 <ali.sayed987@yahoo.com>
2024-01-26 21:46:59 +0200

    register tables models relationships on startup

--- backend/database/db.js
+++ backend/database/db.js
@@ -8,6 +8,23 @@ const sequelize = new Sequelize(config.db.database, config.db.user, config.db.pa
   dialect: 'mysql'
 });
 
+/**
+ * Register all associations using the models in the "/models" directory
+ */
+let modelsArr = [];
+fs
+  .readdirSync(__dirname + "\\models")
+  .forEach(file => {
+    const model = require(`./models/${file}`)(sequelize, Sequelize.DataTypes);
+    modelsArr[model.name] = model;
+  });
+
+Object.keys(modelsArr).forEach(modelName => {
+  if (modelsArr[modelName].associate) {
+    modelsArr[modelName].associate(modelsArr);
+  }
+});
+
 sequelize.authenticate()
   .then(() => console.log('Connection has been established'))
   .catch((error) => console.error('Unable to connect to the database:', error))

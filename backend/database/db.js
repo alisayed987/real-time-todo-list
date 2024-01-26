c26e3c620a21e3ccc20dc5dd38cd65aeb71ae933
alisayed987 <ali.sayed987@yahoo.com>
2024-01-26 18:23:04 +0200

    add db ENV & configs then create db connections

---
+++ backend/database/db.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+const config = require('../config.js')
+
+//-----------< Create DB connection >------------------------------
+const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
+  host: config.db.host,
+  port: config.db.port,
+  dialect: 'mysql'
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Connection has been established'))
+  .catch((error) => console.error('Unable to connect to the database:', error))
+
+module.exports = sequelize;

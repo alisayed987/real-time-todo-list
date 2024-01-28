a739c35cf3ecba16d84c33f3ef9414f60be5cb04
alisayed987 <ali.sayed987@yahoo.com>
2024-01-28 04:46:16 +0200

    install react router and register basic routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import socketIOClient from 'socket.io-client';
-import TodoList from './components/TodoList';
-import AddTodo from './components/AddTodo';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import TodoApp from "./components/TodoApp";
+import ProtectedRoute from "./components/ProtectedRoute";
 import './App.css';
 
-
 function App() {
-
   return (
-    <div className="app">
-      <div className="header">
-        <h1>Todo List</h1>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/todo" element={<ProtectedRoute component={TodoApp} />} />
+      </Routes>
+    </Router>
   );
 }
 
